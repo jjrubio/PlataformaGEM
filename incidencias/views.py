bac78bac938cc91fc8ab78dc776baa2d4eea058c
jjrubio <jefferson.jrubio@gmail.com>
2014-05-17 11:38:41 -0500

    add media, js an templates folders

--- incidencias/views.py
+++ incidencias/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from models import *
 
-# Create your views here.
+
+def mapa_incidencias(request):
+    categorias = Categoria.objects.all()
+    incidencias = Incidencia.objects.all()
+    incidencias_info = Incidencia_info.objects.all()
+    template = "mapa_incidencias.html"
+    return render_to_response(template, context_instance = RequestContext(request,locals()))
