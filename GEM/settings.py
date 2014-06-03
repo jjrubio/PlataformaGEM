0ec6126a4bbf7165bb34732c4181c29c43921f6c
jjrubio <jefferson.jrubio@gmail.com>
2014-06-03 12:07:15 -0500

    remove test heroku

--- GEM/settings.py
+++ GEM/settings.py
@@ -12,18 +12,18 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # import os
 # BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
--- GEM/wsgi.py
+++ GEM/wsgi.py
@@ -13,4 +13,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GEM.settings")
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
